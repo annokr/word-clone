32a2fa33052d66a30fea362f63adef73e231e297
Anna Kruczkowska-Wolnik <annokr1@gmail.com>
2023-03-22 20:59:55 +0100

    Excercise 2

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,27 +1,29 @@
 import React from "react";
 
-function GuessInput() {
-  const [word, setWord] = React.useState("");
-
+function GuessInput({ handleSubmitGuess }) {
+  
+  const [tentativeGuess, setTentativeGuess] = React.useState("");
+  
+  function handleSubmit(event) {
+    event.preventDefault();
+    handleSubmitGuess(tentativeGuess);
+    setTentativeGuess("");
+  }
     return (
       <form className="guess-input-wrapper"
-        onSubmit={event => {
-        event.preventDefault();
-        console.log({word});
-        setWord("")
-        }}
+        onSubmit={handleSubmit}
       >
         <label htmlFor="guess-input">Enter guess:</label>
         <input 
           id="guess-input" 
           type="text"
-          value={word}
+          value={tentativeGuess}
           required
           minLength={5}
           maxLength={5}
           pattern="[a-zA-Z]{5}"
           title="5 letter word"
-          onChange={event => {setWord(event.target.value.toUpperCase())}}
+          onChange={event => {setTentativeGuess(event.target.value.toUpperCase())}}
         />
       </form>
     )
